52f022bfecc2386c461ea74b37a169a9c75afd15
Abdul Rehan <100872683+abrehan2@users.noreply.github.com>
2023-01-27 19:39:04 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register/Register";
 import Otp from './pages/OTP/Otp';
 
-
 const App = () => {
   return (
    <>
 
 <BrowserRouter>
-
 <Routes>
 <Route path='/' element = {<Register/>}/>
 <Route path='/Otp' element = {<Otp/>}/>
-
 </Routes>
-
 </BrowserRouter>
-
-
 
    </>
   );
